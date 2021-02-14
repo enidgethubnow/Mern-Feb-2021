8b7668ba0f41c3bd7f0e86833fd5ee1c9a4262ef
enidgethub <47485051+enidgethub@users.noreply.github.com>
2021-02-13 22:04:33 -0800

    Ninja

---
+++ Ninja/ninja.js
@@ -0,0 +1,32 @@
+
+
+
+class Ninja {
+    constructor(name, health, strength = 3, speed = 3) {
+      this.name = name;
+      this.health = health;
+      this.strength = strength;
+      this.speed = speed;
+    }
+    sayName() {
+      console.log(this.name);
+      return this;
+    }
+    showStats() {
+      console.log(
+        `Health: ${this.health}, Strength: ${this.strength}, Speed: ${this.speed}`
+      );
+      return this;
+    }
+
+    drinkSake() {
+      this.health += 10;
+      return this;
+    }
+  }
+  
+  const Enid = new Ninja("Enid Peterson", 50);
+  Enid.showStats();
+  Enid.drinkSake();
+  
+
