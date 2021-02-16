8d3d8014ffbc5c95521bb481b86f9acea750d5d4
enidgethub <47485051+enidgethub@users.noreply.github.com>
2021-02-15 20:28:38 -0800

    Reactjsx

---
+++ reactjsx/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
